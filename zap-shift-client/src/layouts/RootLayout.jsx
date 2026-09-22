d632192441dc7eb14bb7a318ce29be08244c1b1f
Nahid <mdnahidhosentuhin@gmail.com>
2026-09-22 13:46:17 +0600

    Fix project repository structure

---
+++ zap-shift-client/src/layouts/RootLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const RootLayout = () => {
+    return (
+        <div>
+            <Outlet>
+
+            </Outlet>
+        </div>
+    );
+};
+
+export default RootLayout;
