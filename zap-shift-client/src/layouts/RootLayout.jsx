97d96b9fdb3290000ad88e7ddf64d76cb5c2b3b6
Nahid <mdnahidhosentuhin@gmail.com>
2026-09-22 14:34:42 +0600

    NavBar and Footer sections have been added

--- zap-shift-client/src/layouts/RootLayout.jsx
+++ zap-shift-client/src/layouts/RootLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../pages/shared/Footer/Footer';
+import NavBar from '../pages/shared/NavBar/NavBar';
 
 const RootLayout = () => {
     return (
         <div>
-            <Outlet>
-
-            </Outlet>
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
