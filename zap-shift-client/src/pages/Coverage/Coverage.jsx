246b10fc5117cab072f478205b640a7805712ea3
Nahid <mdnahidhosentuhin@gmail.com>
2026-09-26 23:49:03 +0600

    Use Map using react leaflet to Display all the service area marker

--- zap-shift-client/src/pages/Coverage/Coverage.jsx
+++ zap-shift-client/src/pages/Coverage/Coverage.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const Coverage = () => {
-    return (
-        <div>
-            <h3>We are available in 64 districts</h3>
-        </div>
-    );
+  const position = [23.6850, 90.3563]; 
+
+  return (
+    <div>
+      <h2 className="text-5xl">We are available in 64 districts</h2>
+      <div></div>
+      <div  className="border w-full h-[800px]">
+        <MapContainer center={position} zoom={7} scrollWheelZoom={false} className="w-full h-full">
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </div>
+  );
 };
 
 export default Coverage;
